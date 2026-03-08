2241523760bb87e51c8414a2ed225d6e2ec01595
abebekebede11223344-netizen <abebekebede11223344@gmail.com>
2026-03-08 14:32:09 +0300

    Refactor service worker event listeners for clarity

--- sw.js
+++ sw.js
@@ -3,21 +3,24 @@ const assets = ['./', './index.html', './style.css', './app.js', './manifest.jso
 
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
-    })
-  );
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
+  }));
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).then(fetchRes => {
+        return caches.open(cacheName).then(cache => {
+          cache.put(e.request.url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    })
   );
 });
