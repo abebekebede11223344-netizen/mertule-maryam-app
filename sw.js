0668126d519bc83f3dc8401bcf6ffd32e75e9ead
abebekebede11223344-netizen <abebekebede11223344@gmail.com>
2026-03-08 14:09:08 +0300

    Refactor service worker for dynamic cache name and logic

--- sw.js
+++ sw.js
@@ -1,35 +1,23 @@
-const cacheName = 'mertule-v2';
-const assets = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
-];
+const cacheName = 'mertule-v' + new Date().getTime();
+const assets = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
-// አፑ ሲጫን ፋይሎቹን መቅበር
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('Caching all assets...');
-      return cache.addAll(assets);
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
     })
   );
 });
 
-// ኢንተርኔት በሌለበት ጊዜ ከቀብር (Cache) አውጥቶ ማሳየት
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request).then(fetchRes => {
-        return caches.open(cacheName).then(cache => {
-          // አዲስ የወረዱ ፋይሎችን (PDF/Images) ለወደፊት ኦፍላይን እንዲሆኑ መቅበር
-          cache.put(e.request.url, fetchRes.clone());
-          return fetchRes;
-        });
-      });
-    }).catch(() => {
-        // ኢንተርኔትም ከሌለ Cache ውስጥም ከሌለ የሚታይ (አማራጭ)
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
