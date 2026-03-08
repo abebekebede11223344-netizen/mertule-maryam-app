311b1fc307581365edd46ed7b94fbdc6f77d40c5
abebekebede11223344-netizen <abebekebede11223344@gmail.com>
2026-03-08 13:31:30 +0300

    Upgrade service worker to cache version v2

--- sw.js
+++ sw.js
@@ -1,10 +1,35 @@
-const cacheName = 'mertule-v1';
-const assets = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+const cacheName = 'mertule-v2';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
 
+// አፑ ሲጫን ፋይሎቹን መቅበር
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching all assets...');
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// ኢንተርኔት በሌለበት ጊዜ ከቀብር (Cache) አውጥቶ ማሳየት
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request).then(fetchRes => {
+        return caches.open(cacheName).then(cache => {
+          // አዲስ የወረዱ ፋይሎችን (PDF/Images) ለወደፊት ኦፍላይን እንዲሆኑ መቅበር
+          cache.put(e.request.url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    }).catch(() => {
+        // ኢንተርኔትም ከሌለ Cache ውስጥም ከሌለ የሚታይ (አማራጭ)
+    })
+  );
 });
